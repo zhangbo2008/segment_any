c246f64cdeeff0a4cf7a8af3649428e8cf24b052
zhangbo2008 <15122306087@163.com>
2023-04-19 11:38:36 +0800

    no_comment

---
+++ 1.py
@@ -0,0 +1,10 @@
+
+#=========输入提示词,来获得maks图片.
+pat='E:/sam_vit_h_4b8939.pth'
+
+promp=''
+from segment_anything import SamPredictor, sam_model_registry
+sam = sam_model_registry["default"](checkpoint=pat)
+predictor = SamPredictor(sam)
+predictor.set_image('demo.jpg')
+masks, _, _ = predictor.predict(<input_prompts>)
---
+++ 2.py
@@ -0,0 +1,18 @@
+
+#=========输入提示词,来获得maks图片.
+pat='/mnt/e/sam_vit_h_4b8939.pth'
+
+promp=''
+from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
+sam = sam_model_registry["default"](checkpoint=pat)
+mask_generator = SamAutomaticMaskGenerator(sam)
+
+import cv2
+image = cv2.imread('demo.jpg')
+
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+
+
+masks = mask_generator.generate(image)
+print(1)
