fe41fff44216b9c5d460b8039fab5cf3837669f3
zhangbo2008 <15122306087@163.com>
2023-04-20 13:07:33 +0800

    no_comment

--- fuwu.py
+++ fuwu.py
@@ -14,7 +14,7 @@ CORS(app)  # 解决跨域问题
 
 
 #=========输入提示词,来获得maks图片.
-pat='/mnt/e/sam_vit_h_4b8939.pth'
+pat='/sam_vit_h_4b8939.pth'
 
 promp=''
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
